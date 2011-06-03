c327a2881e7725facdf5ad5f967345c98d1a3ffe
Daniel Hahn <dha@betterplace.org>
2011-06-03 21:09:40 +0200

    Initial checkin

---
+++ slippymapPlugIn.h
@@ -0,0 +1,24 @@
+//
+//  slippymapPlugIn.h
+//  slippymap
+//
+//  Created by Daniel Hahn on 03.06.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface slippymapPlugIn : QCPlugIn
+{
+}
+
+/*
+Declare here the Obj-C 2.0 properties to be used as input and output ports for the plug-in e.g.
+@property double inputFoo;
+@property(assign) NSString* outputBar;
+You can access their values in the appropriate plug-in methods using self.inputFoo or self.inputBar
+*/
+
+@property(assign) id<QCPlugInOutputImageProvider> outputImage;
+
+@end
